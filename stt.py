e0f957ae532e533e1bcf3b99a31ee02388fbc779
Krutik Patel <krutikpatel02@gmail.com>
2021-10-23 23:27:21 +0530

    Create stt.py

---
+++ stt.py
@@ -0,0 +1,24 @@
+# import module
+import speech_recognition as sr
+
+# initialize
+r=sr.Recognizer()
+
+# convert speech to text 
+while True:
+    with sr.Microphone() as source:
+        # clear background noise
+        r.adjust_for_ambient_noise(source, duration=0.3)
+
+        print("Speak now..")
+
+        # capture the audio
+        audio = r.listen(source)
+
+        try:
+            text = r.recognize_google(audio)
+            print("Speaker:",text)
+            if text=="wait":
+                break
+        except:
+            print("Please say again !")
